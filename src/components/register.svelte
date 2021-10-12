d53ccdfe9802511727c73083cdd5374d739b9817
deficts <diegoar7@live.com>
2021-10-11 21:11:38 -0500

    Starting auth templates

---
+++ src/components/register.svelte
@@ -0,0 +1,41 @@
+<script>
+	import { TextInput } from 'carbon-components-svelte';
+	import { Button } from 'carbon-components-svelte';
+</script>
+
+<div class="login-box">
+	<h1 style="margin-bottom: 16px;">Bienvenido</h1>
+	<TextInput
+		labelText="Email"
+		placeholder="Ingresar tu email..."
+		type="email"
+		style="margin-bottom: 16px;"
+	/>
+	<TextInput
+		labelText="Nombre de Hogar"
+		placeholder="Ingresar el nombre de tu hogar..."
+		type="text"
+		style="margin-bottom: 16px;"
+	/>
+	<TextInput
+		labelText="Contraseña"
+		placeholder="Ingresa tu contraseña..."
+		type="password"
+		style="margin-bottom: 16px;"
+	/>
+	<TextInput
+		labelText="Repetir Contraseña"
+		placeholder="Ingresa tu contraseña..."
+		type="password"
+	/>
+	<Button style="margin-top: 16px; float: right;">Registrarme</Button>
+	<Button style="margin-top: 16px; float: right;" kind="secondary" href="/">Ingresar</Button>
+</div>
+
+<style>
+	.login-box {
+		padding: 3rem;
+		width: 50%;
+		margin: 0 auto;
+	}
+</style>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,4 +1,39 @@
-<slot />
+<script>
+	import {
+		Header,
+		SideNav,
+		SideNavItems,
+		SideNavLink,
+		SkipToContent,
+		Content,
+		HeaderNav,
+		HeaderNavItem
+	} from 'carbon-components-svelte';
+
+	let isSideNavOpen = false;
+</script>
+
+<Header company="Soldados de Vini" platformName="Community Hub" bind:isSideNavOpen>
+	<div slot="skip-to-content">
+		<SkipToContent />
+	</div>
+
+	<HeaderNav>
+		<HeaderNavItem href="/register" text="Registrarme" />
+		<HeaderNavItem href="/" text="Ingresar" />
+	</HeaderNav>
+
+	<SideNav bind:isOpen={isSideNavOpen} rail>
+		<SideNavItems>
+			<SideNavLink text="Mi Hogar" href="/hogar" />
+			<SideNavLink text="Mi Comunidad" href="/comunidad" />
+		</SideNavItems>
+	</SideNav>
+</Header>
+
+<Content>
+	<slot />
+</Content>
 
 <style lang="scss">
 	@import 'layout.scss';
