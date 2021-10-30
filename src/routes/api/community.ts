5ab6e67a61635122155cc60142bc948f0e84122b
Vinicio Quintero <vinicioquinterobuss@gmail.com>
2021-10-29 22:21:27 -0600

    Added auth validation on community endpoints

--- src/routes/api/community.ts
+++ src/routes/api/community.ts
@@ -2,12 +2,22 @@ import type { EndpointOutput } from '@sveltejs/kit';
 import { getCommunities, getSingleCommunity, insertCommunity } from '$lib/db/community_lib'
 import { DEFAULT_ERROR_REQUEST } from '$lib/errors';
 import { isCreateCommunity } from '$lib/dto/community';
+import { verifyToken } from '$lib/auth_guard';
 
-export async function get(): Promise<EndpointOutput> {
+export async function get({ headers }): Promise<EndpointOutput> {
+	const error = verifyToken(headers);
+	if (error != null) {
+		return error;
+	}
 	return { body: await getCommunities() };
 }
 
-export async function post({ body }): Promise<EndpointOutput> {
+export async function post({ body, headers }): Promise<EndpointOutput> {
+	// TODO(hivini): Verify only admins can do this.
+	const error = verifyToken(headers);
+	if (error != null) {
+		return error;
+	}
 	if (isCreateCommunity(body)) {
 		let resp = await insertCommunity(body);
 		let community = await getSingleCommunity(resp["insertId"]);
