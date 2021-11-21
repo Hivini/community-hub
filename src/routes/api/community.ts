95a4e23aec11cadeadab71f33470f3135a5108ef
Vinicio Quintero <vinicioquinterobuss@gmail.com>
2021-11-21 11:49:19 -0700

    Removed guards from GET & POST of api/community

--- src/routes/api/community.ts
+++ src/routes/api/community.ts
@@ -2,21 +2,12 @@ import type { EndpointOutput } from '@sveltejs/kit';
 import { getCommunities, getSingleCommunity, insertCommunity } from '$lib/db/community_lib';
 import { DEFAULT_ERROR_REQUEST } from '$lib/errors';
 import { isCreateCommunity } from '$lib/dto/community';
-import { verifyToken } from '$lib/auth_guard';
 
-export async function get({ headers }): Promise<EndpointOutput> {
-	const error = verifyToken(headers);
-	if (error != null) {
-		return error;
-	}
+export async function get(): Promise<EndpointOutput> {
 	return { body: await getCommunities() };
 }
 
-export async function post({ body, headers }): Promise<EndpointOutput> {
-	const error = verifyToken(headers);
-	if (error != null) {
-		return error;
-	}
+export async function post({ body }): Promise<EndpointOutput> {
 	if (isCreateCommunity(body)) {
 		let resp = await insertCommunity(body);
 		let community = await getSingleCommunity(resp['insertId']);
