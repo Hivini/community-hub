360385f99c0664d890269d954e315f55b46c7141
Diego <diegoar7@live.com>
2021-11-20 18:23:35 -0600

    Fetching user data on login and register

---
+++ src/routes/community/index.svelte
@@ -0,0 +1,10 @@
+<script>
+	import { Button } from 'carbon-components-svelte';
+
+	function logout() {
+		localStorage.clear();
+		window.location.reload();
+	}
+</script>
+
+<Button style="margin-top: 16px;" kind="danger" on:click={logout}>Cerrar Sesión</Button>
